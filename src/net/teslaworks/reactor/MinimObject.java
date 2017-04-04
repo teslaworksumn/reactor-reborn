991adc6f5a325737caba9ba838269fc29585284c
Ryan Fredlund <rfredlund13@gmail.com>
2017-04-03 23:12:13 -0500

    Made MinimObject functions public (so they can actually be used/found)

--- src/net/teslaworks/reactor/MinimObject.java
+++ src/net/teslaworks/reactor/MinimObject.java
@@ -6,14 +6,14 @@ public class MinimObject {
     /*
     Generate an absolute path from the file name provided
     */
-    String sketchPath(String fileName) {
+    public String sketchPath(String fileName) {
         return null;
     }
     
     /*
     Get an input stream for a given file name (not necessarily an absolute path)
     */
-    InputStream createInput(String fileName) {
+    public InputStream createInput(String fileName) {
         return null;
     }
 }
