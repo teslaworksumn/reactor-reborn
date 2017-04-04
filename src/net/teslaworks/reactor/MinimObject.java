c913e3c1c464121955b9bc77ae72676cfe4c5820
Ryan Fredlund <rfredlund13@gmail.com>
2017-04-03 23:08:15 -0500

    Initial files. Setup structure to use Minim, but not implemented

---
+++ src/net/teslaworks/reactor/MinimObject.java
@@ -0,0 +1,19 @@
+package net.teslaworks.reactor;
+
+import java.io.InputStream;
+
+public class MinimObject {
+    /*
+    Generate an absolute path from the file name provided
+    */
+    String sketchPath(String fileName) {
+        return null;
+    }
+    
+    /*
+    Get an input stream for a given file name (not necessarily an absolute path)
+    */
+    InputStream createInput(String fileName) {
+        return null;
+    }
+}
---
+++ src/net/teslaworks/reactor/Reactor.java
@@ -0,0 +1,14 @@
+package net.teslaworks.reactor;
+
+import ddf.minim.Minim;
+
+public class Reactor {
+
+	public static void main(String[] args)
+    {
+	    System.out.println("Here");
+	    MinimObject minimObject = new MinimObject();
+	    Minim minim = new Minim(minimObject);
+	}
+
+}
