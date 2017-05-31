6b561bbf4b2230f2d94c8445703979baf77307a5
Kayla Engelstad <engel813@umn.edu>
2017-05-30 20:11:22 -0500

    MinimObject stuff (#4)

--- src/net/teslaworks/reactor/MinimObject.java
+++ src/net/teslaworks/reactor/MinimObject.java
@@ -1,9 +1,11 @@
 package net.teslaworks.reactor;
 
 import java.io.InputStream;
+import java.io.FileInputStream;
 import java.nio.file.FileSystems;
 import java.io.IOException; //thrown by toRealPath()
 import java.nio.file.NoSuchFileException; //thrown by toRealPath()
+import java.io.FileNotFoundException; //thrown by FileInputStream obj creation
 import java.nio.file.Path;
 
 public class MinimObject {
@@ -12,7 +14,6 @@ public class MinimObject {
     */
     public String sketchPath(String fileName) {
         
-        String absolute_path;
         Path real_path = null;
         Path path = FileSystems.getDefault().getPath(fileName);
         
@@ -20,22 +21,30 @@ public class MinimObject {
             real_path = path.toRealPath();
         } 
         catch (NoSuchFileException x) {
-            System.err.format("%s: no such" + " file or directory%n", path);
+            System.err.format("%s: no such" + " file or directory\n", path);
             // Logic for case when file doesn't exist.
         } 
         catch (IOException x) {
-            System.err.format("%s%n", x);
+            System.err.format("%s\n", x);
             // Logic for other sort of file error.
         }
-        absolute_path = real_path.toString();
         
-        return absolute_path;
+        return real_path.toString();
     }
     
     /*
     Get an input stream for a given file name (not necessarily an absolute path)
     */
     public InputStream createInput(String fileName) {
-        return null;
+        InputStream inputstream = null;
+        String full_name = sketchPath(fileName);
+        
+        try {
+            inputstream = new FileInputStream(full_name);
+        }
+        catch (FileNotFoundException x) {
+            System.err.format("%s\n", x);
+        }
+        return inputstream;
     }
 }
